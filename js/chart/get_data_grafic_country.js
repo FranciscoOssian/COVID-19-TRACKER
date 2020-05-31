49dad32e90d7be29cdebb8889e381e9ee5e3ace9
Francisco Ossianl.com <ossian.neto@gmail.com>
2020-05-31 20:22:02 -0300

    stage 3

--- js/chart/get_data_grafic_country.js
+++
@@ -1,7 +0,0 @@
-function get_data_grafic_country (country_name) {
-    return (
-        get_json("https://pomber.github.io/covid19/timeseries.json")
-            .then( data => data[country_name] )
-            .catch( err => console.log(err))
-    )
-}
--- js/firebase.js
+++
@@ -1,7 +0,0 @@
-get_json("https://franciscoossianfoln.github.io/COVID-19-TRACKER/config.json")
-    .then( config => {
-        // Initialize Firebase
-        firebase.initializeApp(config);
-        firebase.analytics();
-
-    } )
